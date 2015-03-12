adab4707f74bb538fb7fb4a2a96c8e886bd6c5d2
hyunghunny <hyunghunny@gmail.com>
2015-03-12 13:52:44 +0900

    add sample for twitter open api

---
+++ python/twitter/credential.py
@@ -0,0 +1,20 @@
+# Secret information for connecting twitter
+# author: webofthink@snu.ac.kr
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+
+def getOAuthToken() :
+	return OAUTH_TOKEN
+
+def getOAuthSecret() :
+	return OAUTH_TOKEN_SECRET
+
+def getConsumerKey() :
+	return CONSUMER_KEY
+
+def getConsumerSecret() :
+	return CONSUMER_SECRET
